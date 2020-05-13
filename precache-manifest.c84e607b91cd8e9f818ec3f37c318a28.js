a69ffe08c1ecc069f2de5fcd128ea26da0fb5cc0
riyandharne-123 <rockydods123@gmail.com>
2020-05-13 21:55:08 +0530

    Updates

---
+++ precache-manifest.c84e607b91cd8e9f818ec3f37c318a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d9d5357024cfaaf8f456cb63fe3c45",
+    "url": "/Preplaced1/index.html"
+  },
+  {
+    "revision": "38b5d44218ff46aa9a03",
+    "url": "/Preplaced1/static/css/main.7c5674b9.chunk.css"
+  },
+  {
+    "revision": "519d17497a1bb9bd368b",
+    "url": "/Preplaced1/static/js/2.fb31a14d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Preplaced1/static/js/2.fb31a14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b5d44218ff46aa9a03",
+    "url": "/Preplaced1/static/js/main.6e11237c.chunk.js"
+  },
+  {
+    "revision": "62ab9f8e43835b1ef527",
+    "url": "/Preplaced1/static/js/runtime-main.f3118466.js"
+  }
+]);
